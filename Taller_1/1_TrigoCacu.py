f7dd4362b55d38156b3a60eb245704f5bbaea0ba
Hokhmah7 <alfred106moreno@gmail.com>
2026-09-10 15:54:04 +0000

    Menu

---
+++ Taller_1/1_TrigoCacu.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+#Menu repetible
+
+opciones = 1
+
+while opciones < 4: 
+    print("Escoge una opcion:")
+    print("1. Seno")
+    print("2. Coseno")
+    print("3. Tangente")
+    print("Salir")    
+    
+    opciones = int(input("Ingrese una opcion: "))
+
+#Grados a radianes
+
+
+#Calculo del seno
+
+
+
+#Calculo del coseno
